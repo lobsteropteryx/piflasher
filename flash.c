a8183903d7170c9739ed3045306fefab37b496c2
Chris Lasher <chris.lasher@gmail.com>
2015-02-07 15:57:13 -0800

    Properly cleans up after terminating the program.
    Handles the SIGINT (Ctrl+C) from the user to terminate the program.
    Ensures pins are set back to original read states before exiting.

--- flash.c
+++ flash.c
@@ -1,10 +1,54 @@
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <unistd.h>
+
 #include <wiringPi.h>
 
+// Broadcom numbering
 const int LED_PIN = 2;
 const int BUTTON_PIN = 3;
 
-void flash(int num_flashes) {
+static bool KEEP_RUNNING = true;
+
+
+void handle_button_press(void);
+void flash_led(int);
+void handle_SIGINT(int);
+void setup(void);
+void cleanup(void);
+
+
+int main(void) {
+  setup();
+
+  wiringPiISR(BUTTON_PIN, INT_EDGE_FALLING, &handle_button_press);
+  signal(SIGINT, handle_SIGINT);
+
+  while (KEEP_RUNNING) {
+    sleep(1);
+  }
+
+  cleanup();
+  return 0;
+}
+
+void setup(void) {
+  wiringPiSetupGpio();
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  pullUpDnControl(BUTTON_PIN, PUD_UP);
+
+  digitalWrite(LED_PIN, LOW);
+}
+
+
+void handle_button_press(void) {
+  flash_led(4);
+}
+
+
+void flash_led(int num_flashes) {
   int i;
   for (i = 0; i < num_flashes; i++) {
     delay(100);
@@ -14,26 +58,13 @@ void flash(int num_flashes) {
   }
 }
 
-void handle_interrupt(void) {
-    flash(4);
+
+void handle_SIGINT(int signal) {
+  KEEP_RUNNING = false;
 }
 
-int main(void) {
 
-  //Use Broadcom numbering
-  wiringPiSetupGpio();
-
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
-  pullUpDnControl(BUTTON_PIN, PUD_UP);
-
-  digitalWrite(LED_PIN, LOW);
-
-  wiringPiISR(BUTTON_PIN, INT_EDGE_FALLING, &handle_interrupt);
-
-  while (1) {
-    sleep(1);
-  }
-
-  return 0;
+void cleanup(void) {
+  pinMode(LED_PIN, INPUT);
+  pullUpDnControl(BUTTON_PIN, PUD_OFF);
 }
